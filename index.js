ef84b155ab3a88c51f97b0f07789d6a36c7b32e1
Dmitry Mayorov <iamdmitrymayorov@gmail.com>
2016-01-31 14:47:52 +0500

    Code cleanup.

--- index.js
+++ index.js
@@ -5,27 +5,32 @@ var fs   = require( 'fs' );
 var path = require( 'path' );
 
 // Process user arguments.
-var userArgs = process.argv.slice( 2 );
-	newSlug  = userArgs[0];
-	newName  = userArgs[1];
+var args = process.argv.slice( 2 );
 
-// Current theme details.
-var themeDir, stylesheet, oldSlug, oldName;
+//Define variables.
+var oldSlug,
+	oldName,
+	newSlug,
+	newName,
+	themeDir,
+	stylesheet;
 
 // Excluded folders.
-var excDir = [
-	'./node_modules',
-	'./fonts',
-	'./assets/fonts'
+var folderExclude = [
+	'node_modules',
+	'.git',
+	'.sass-cache',
+	'.hg',
+	'.svn',
+	'.CVS',
+	'cache',
 ]
 
 // Excluded files.
-var excFile = [
-	'Gruntfile.js',
-]
+var fileExclude = []
 
 // File extensions to work with.
-var incExt = [
+var extensions = [
 	'.php',
 	'.html',
 	'.js',
@@ -38,12 +43,42 @@ var incExt = [
 	'.pot'
 ]
 
+// Process arguments.
+for ( var i = 0; i < args.length; i++ ) {
+	if ( args[ i ].indexOf( '-s' ) != -1 ) {
+		newSlug = args[ i ].replace( '-s=', '' );
+	} else if ( args[ i ].indexOf( '-n' ) != -1 ) {
+		newName = args[ i ].replace( '-n=', '' );
+	} else if ( args[ i ].indexOf( '-extensions' ) != -1 ) {
+		extensions = args[ i ].replace( '-extensions=', '' ).split( ',' );
+	} else if ( args[ i ].indexOf( '-folderExclude' ) != -1 ) {
+		folderExclude = args[ i ].replace( '-folderExclude=', '' ).split( ',' );
+	} else if ( args[ i ].indexOf( '-fileExclude' ) != -1 ) {
+		fileExclude = args[ i ].replace( '-fileExclude=', '' ).split( ',' );
+	}
+}
+
+// Return if no slug is provided.
+if ( ! newSlug ) {
+	console.log( 'No slug provided!' );
+	return;
+}
+
+// Return in no name is provided.
+if ( ! newName ) {
+	console.log( 'No name provided!' );
+	return;
+}
+
 // Read the themeDirectory.
 themeDir = fs.readdirSync( '.' );
 
 // If we have stylesheet, read it.
 if ( typeof themeDir == 'object' && themeDir.indexOf( 'style.css' ) != -1 ) {
 	stylesheet = fs.readFileSync( 'style.css', 'UTF8' );
+} else {
+	console.log( 'No style.css in the folder!' );
+	return
 }
 
 // If stylesheet is not empty read the theme slug.
@@ -53,6 +88,9 @@ if ( stylesheet && stylesheet.search( /Text Domain:.*$/m ) != -1 ) {
 
 	// Trim everything except the slug.
 	oldSlug = oldSlug[0].replace( 'Text Domain:', '' ).trim();
+} else {
+	console.log( 'Text domain is not defined!' );
+	return
 }
 
 // If stylesheet is not empty read the Theme Name.
@@ -62,12 +100,44 @@ if ( stylesheet && stylesheet.search( /Theme Name:.*$/m ) != -1 ) {
 
 	// Trim everything except the Name.
 	oldName = oldName[0].replace( 'Theme Name:', '' ).trim();
+}  else {
+	console.log( 'Theme name is not defined!' );
+	return
+}
+
+// Check for a dot and and a slash at the beginning of the folder name and add it if necessary.
+if ( folderExclude.length > 0 ) {
+	for ( var i = 0; i < folderExclude.length; i++ ) {		
+		if ( folderExclude[i].indexOf( './' ) != 0 )
+			folderExclude[i] = './' + folderExclude[i];
+	};
+}
+
+// Check for a dot and and a slash at the beginning of the file name and add it if necessary.
+if ( fileExclude.length > 0 ) {
+	for ( var i = 0; i < fileExclude.length; i++ ) {		
+		if ( fileExclude[i].indexOf( './' ) != 0 )
+			fileExclude[i] = './' + fileExclude[i];
+	};
+}
+
+// Check for a dot at the beginning of the extension name and add it if necessary.
+if ( extensions.length > 0 ) {
+	for ( var i = 0; i < extensions.length; i++ ) {		
+		if ( extensions[i].indexOf( '.' ) != 0 )
+			extensions[i] = '.' + extensions[i];
+	};
 }
 
 // Replace tasks.
 function replaceStrings( file ) {
+
 	// Get the content of the file.
 	var content = fs.readFileSync( file, 'UTF8' );
+
+	console.log( oldSlug, oldName, newSlug, newName );
+
+	var old = content;
 
 	// Replace the prefixes.
 	content = content.replace(
@@ -147,31 +217,43 @@ function replaceStrings( file ) {
 		'#' + newName
 	);
 
-
+	console.log ( content == old );
 
 	// Write the file.
 	fs.writeFileSync( file, content, 'UTF8' );
+
 }
 
+/**
+ * Walk the folder recursively and call the string replacing function.
+ * @param  {string} dir Directory to walk
+ */
 function walkDir( dir ) {
 
+	// Check if the directory name has a trailing slash and add it if necessary.
 	if ( dir[ dir.length-1 ] != '/' ) {
 		dir = dir.concat('/')
 	}
 
+	// Read all files and folders in the directory.
 	items = fs.readdirSync( dir );
 	
+	// Loop through each of them.
 	items.forEach( function( item ) {
 		if ( fs.statSync( dir + item ).isDirectory() ) {
-			if ( excDir.indexOf( dir + item ) == -1 ) {
+			if ( folderExclude.indexOf( dir + item ) == -1 ) {
 				walkDir( dir + item + '/' );
 			} 
 		} else if ( fs.statSync( dir + item ).isFile() ) {
-			if ( incExt.indexOf( path.extname( dir + item ) ) != -1 ) {
+			if ( ( extensions.indexOf( path.extname( dir + item ) ) != -1 ) && ( fileExclude.indexOf( dir + item ) == -1 ) ) {
 				replaceStrings( dir + item );
 			}
 		}
 	} );
 };
 
+// Unleash the beast.
 walkDir( '.' );
+
+// Now start fresh with a new theme name!
+console.log( 'All done!' );
